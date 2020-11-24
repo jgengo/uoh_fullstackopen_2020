7c7362f84e63b9ddaee87334eac73c6ab4c717d3
Jordane Titus Gengo <jordane@hive.fi>
2020-11-24 19:42:03 +0200

    add part4 step1-2; refactor an app received 'by mail' -_-' lmao

---
+++ part4/models/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const logger = require('../utils/logger')
+const config = require('../utils/config')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
